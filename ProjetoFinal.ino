910b0bd369307ed440ef0763eba34908f5e8d2b7
Guisn25 <127779512+Guisn25@users.noreply.github.com>
2025-02-24 21:54:08 -0300

    Update ProjetoFinal.ino

--- ProjetoFinal.ino
+++ ProjetoFinal.ino
@@ -1,8 +1,14 @@
 #include <Servo.h>
+#include <Wire.h>
 
 Servo servo_motor;
 
 int leitura_Joystick;
+int pos;
+
+float giro_x;
+float giro_y; 
+float giro_z;
 
 void setup() {
   Serial.begin(9600);
@@ -10,17 +16,49 @@ void setup() {
   pinMode(4,OUTPUT);  // TRIG sensor ultrassônico
   pinMode(6,INPUT);   // ECHO sensor ultrassônico
   pinMode(8, OUTPUT);  // BUZZER
-  servo_motor.attach(9); 
-  servo_motor.write(10); 
   
+  servo_motor.attach(10); 
+  servo_motor.write(30);
   
-  digitalWrite(2, LOW);
+  Wire.begin();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B); 
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  digitalWrite(2, HIGH);
 }
+int levanta = 0;
+bool trava = 0;
 
-
-unsigned int Tdist;
+unsigned long Tdist;
 void loop(){
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(0x68, 6, true);
+  giro_x = Wire.read() << 8 | Wire.read();  
+  giro_y = Wire.read() << 8  | Wire.read(); 
+  giro_z = Wire.read() << 8 | Wire.read();
+
+  giro_x = giro_x / 131.0; 
+  giro_y = giro_y / 131.0;
+  giro_z = giro_z / 131.0;
+ 
+  if(analogRead(1) > 550){
+    trava = 1;
+  }else{
+    trava = 0;
+  }
+  if(trava){
+    leitura_Joystick = analogRead(0);
+    pos = map(leitura_Joystick, 0, 1023, 10, 45);
+    servo_motor.write(pos);
+  }
+
   if(millis()-Tdist > 500){
+    
     int distancia = sensor_proximidade(4,6);
     if(distancia <= 20){
       tone(8, 2000);
@@ -28,10 +66,8 @@ void loop(){
     else{
       noTone(8);
     }
-    Serial.println(distancia);
     Tdist = millis();
   }
-
 }
 
 int sensor_proximidade(int pinotrig,int pinoecho){
@@ -40,6 +76,6 @@ int sensor_proximidade(int pinotrig,int pinoecho){
   digitalWrite(pinotrig,HIGH);
   delayMicroseconds(10);
   digitalWrite(pinotrig,LOW);
-
-  return pulseIn(pinoecho,HIGH)/58;
+  int aux = pulseIn(pinoecho,HIGH)/58;
+  return aux;
 }
